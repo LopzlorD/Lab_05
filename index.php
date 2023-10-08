cdd388f67ec7a767fcbf0f53fe896722a5f2e322
LopzlorD <143763145+LopzlorD@users.noreply.github.com>
2023-10-08 13:07:56 -0500

    Propuesto_1

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Calculadora de Gaseosas</title>
+    <title>Calculadora de Sueldo</title>
 </head>
 <body>
-    <h1>Calculadora de Gaseosas</h1>
     <form method="get" action="calculadora.php">
-        Cantidad de gaseosas: <input type="text" name="cant_gaseosa">
+        <label for="total_vendido">Total vendido:</label>
+        <input type="text" name="total_vendido" id="total_vendido" required>
         <br>
-        Precio de gaseosa: <input type="text" name="precio_gaseosa">
+        <label for="cant_hijos">Cantidad de hijos:</label>
+        <input type="text" name="cant_hijos" id="cant_hijos" required>
         <br>
-        <input type="submit" value="Calcular">
+        <label for="sueldo_basico">Sueldo basico:</label>
+        <input type="text" name="sueldo_basico" id="sueldo_basico" required>
+        <br>
+        <input type="submit" value="Calcular Sueldo">
     </form>
 </body>
 </html>
